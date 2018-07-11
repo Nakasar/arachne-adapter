2f47b40263947bce2c28322d41b8d5c6a7b518ee
THIZY Kevin <kevin.thizy@outlook.fr>
2018-07-11 16:22:19 +0200

    Receive message, transmit to brain, get response.

---
+++ index.js
@@ -0,0 +1,13 @@
+/*!
+ *
+ *  ARACHNE GENERIC LIBRARY FOR ADAPTERS
+ *   JAVASCRIPT
+ *
+ * Copyright(c) 2018 Kevin 'Nakasar' Thizy
+ * Copyright(c) 2018 Intech SA
+ * MIT Licensed
+*/
+
+'use strict';
+
+module.exports = require('./lib/arachne');
